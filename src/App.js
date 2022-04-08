424d42d90e0ec93ca2e57c2cad089ce072eff23e
mitulmj <mj695067@gmail.com>
2022-04-08 10:15:15 +0530

    complete add user flow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import AddUser from "./components/User/AddUser";
-
+import './app.css';
+import { useState } from "react";
 const App = () => {
+    const [user,setUser] = useState([])
+    const addUser = (data) =>{
+        setUser((previosState) => {return [data,...previosState]})
+        console.log(user)
+    }
     return(
         <div>
-            <AddUser/>
+            <AddUser getUser={addUser}/>
         </div>
     )
 }
--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,31 +1,66 @@
+import { useState } from 'react';
+import Button from '../UI/Button';
+import Card from '../UI/Card';
 import classes from './Add.module.css';
 
-const AddUser = () => {
+const AddUser = (props) => {
+    const [user,setUser] = useState('');
+    const [Age,setAge] = useState('');
+    const [gender,setGender] = useState('Male');
+    const [course,setCourse] = useState('PHP');
+
+    const userChangeHandler = (e) =>{
+        setUser(e.target.value);
+    }
+    const ageChangeHandler = (e) =>{
+        setAge(e.target.value);
+    }
+    const genderChange = (e)=>{
+        setGender(e.target.value)
+    }
+    const courseChange = (e)=>{
+        setCourse(e.target.value);
+    }
+
+    const submitForm = (e) =>{
+        e.preventDefault()
+        const userObj = {
+            id: Math.random()*10,
+            user,
+            Age,
+            gender,
+            course
+        }
+        props.getUser(userObj)
+    }
     return (
-       <form>
-           <div className="form-control">
-               <label htmlFor="name">User Name</label>
-               <input id="name" type="text" />
-           </div>
-           <div className="form-control">
-               <label htmlFor="age">Age</label>
-               <input type="number" id="age" />
-           </div>
-           <div className="form-control">
-               <label htmlFor="male">Male</label>
-               <input type="radio" name="gender" value="Male" id="male"/>
-               <label htmlFor="">Female</label>
-               <input type="radio" name="gender" value="Female" id="female"/>
-           </div>
-           <div className="form-control">
-               <select name="coursename" id="courseName" value>
-                    <option value="PHP">PHP</option>
-                    <option value="SQL">SQL</option>
-                    <option value="Javascript">Javascript</option>
-                    <option value=".NET">.NET</option>
-               </select>
-           </div>
-       </form>
+        <Card className={classes.input}>
+            <form onSubmit={submitForm}>
+                <div className="form-control">
+                    <label htmlFor="name">User Name</label>
+                    <input id="name" type="text" value={user} onChange={userChangeHandler}/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="age">Age</label>
+                    <input type="number" id="age" value={Age} onChange={ageChangeHandler}/>
+                </div>
+                <div className={`form-control ${classes.radio}`}>
+                    <label htmlFor="male">Male</label>
+                    <input type="radio" name="gender" value="Male" checked={gender === 'Male'} id="male" onChange={genderChange}/>
+                    <label htmlFor="female">Female</label>
+                    <input type="radio" name="gender" value="Female" checked={gender=== 'Female'} id="female" onChange={genderChange}/>
+                </div>
+                <div className="form-control">
+                    <select name="coursename" id="courseName" value={course}  onChange={courseChange}>
+                            <option value="PHP">PHP</option>
+                            <option value="SQL">SQL</option>
+                            <option value="Javascript">Javascript</option>
+                            <option value=".NET">.NET</option>
+                    </select>
+                </div>
+                <Button type="submit">Submit</Button>
+            </form>
+       </Card>
     )
 }
 
